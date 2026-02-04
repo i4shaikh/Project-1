0654f22087bfa06dd710704188321920514de3c9
Irfan Shaikh <shaikhirfan.1143@gmail.com>
2026-02-04 23:49:03 +0530

    Added frontend project

---
+++ frontend/src/app/pages/login/login.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
+})
+export class Login {
+  username = '';
+  password = '';
+
+  constructor(private router: Router) { }
+
+  login() {
+    if (this.username && this.password) {
+      this.router.navigate(['/home']);
+    } else {
+      alert('Enter credentials');
+    }
+  }
+}
